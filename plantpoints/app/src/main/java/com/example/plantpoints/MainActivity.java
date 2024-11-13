341a89b05293c045451911b52eff74f96e4b7bb3
MateresDev <74964125+MateresDev@users.noreply.github.com>
2024-11-13 22:41:29 +0100

    Add map

--- plantpoints/app/src/main/java/com/example/plantpoints/MainActivity.java
+++ plantpoints/app/src/main/java/com/example/plantpoints/MainActivity.java
@@ -1,14 +1,92 @@
 package com.example.plantpoints;
 
+import static org.osmdroid.tileprovider.util.StorageUtils.getStorage;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Canvas;
+import android.graphics.Rect;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import org.osmdroid.api.IMapController;
+import org.osmdroid.config.Configuration;
+import org.osmdroid.config.IConfigurationProvider;
+import org.osmdroid.events.MapListener;
+import org.osmdroid.events.ScrollEvent;
+import org.osmdroid.events.ZoomEvent;
+import org.osmdroid.library.BuildConfig;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.views.CustomZoomButtonsController;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.Overlay;
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
+
+public class MainActivity extends AppCompatActivity implements MapListener {
+
+    MapView mapView;
+    IMapController controller;
+    MyLocationNewOverlay myLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        IConfigurationProvider provider = Configuration.getInstance();
+        provider.setUserAgentValue(BuildConfig.LIBRARY_PACKAGE_NAME);
+        provider.setOsmdroidBasePath(getStorage());
+        provider.setOsmdroidTileCache(getStorage());
+
         setContentView(R.layout.activity_main);
+
+        mapView = findViewById(R.id.main_map);
+        mapView.setTileSource(TileSourceFactory.MAPNIK);
+        mapView.getMapCenter();
+        mapView.setMultiTouchControls(true);
+        mapView.getLocalVisibleRect(new Rect());
+
+        myLocation = new MyLocationNewOverlay(new GpsMyLocationProvider(this), mapView);
+        controller = mapView.getController();
+
+        myLocation.enableMyLocation();
+        myLocation.enableFollowLocation();
+        myLocation.setDrawAccuracyEnabled(true);
+        myLocation.runOnFirstFix(() -> runOnUiThread(() ->{
+            controller.setCenter(myLocation.getMyLocation());
+            controller.animateTo(myLocation.getMyLocation());
+        }));
+
+        controller.setZoom(6.0);
+        mapView.getZoomController().setVisibility(CustomZoomButtonsController.Visibility.NEVER);
+
+        mapView.getOverlays().add(myLocation);
+        mapView.addMapListener(this);
+
+        mapView.getOverlayManager().add(new Overlay() {
+            @Override
+            public void draw(Canvas pCanvas, MapView pMapView, boolean pShadow) {
+                super.draw(pCanvas, pMapView, pShadow);
+            }
+
+            @Override
+            public boolean onSingleTapUp(MotionEvent e, MapView mapView) {
+                controller.animateTo(mapView.getMapCenter());
+                String s = mapView.getMapCenter().getLatitude() + " x " + mapView.getMapCenter().getLongitude();
+                Toast.makeText(getApplicationContext(), s, Toast.LENGTH_SHORT).show();
+                return super.onSingleTapUp(e, mapView);
+            }
+        });
+    }
+
+    @Override
+    public boolean onScroll(ScrollEvent event) {
+        return false;
+    }
+
+    @Override
+    public boolean onZoom(ZoomEvent event) {
+        return false;
     }
 }
